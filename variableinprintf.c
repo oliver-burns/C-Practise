f52ed232984946636acd971f3f750312ba9d5de0
Oliver Burns <oliverburns.kernel@gmail.com>
2026-08-20 22:30:52 +0100

    Added todays practise

---
+++ variableinprintf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(){
+    int num = 15;
+    char letter = 'O';
+
+    printf("I am %d years old, my favourite letter is %c\n", num, letter);
+
+    return 0;
+}
---
+++ variablesdemo.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+
+int num = 15;
+float floatNum = 5.72;
+char letter = 'A';
+
+printf("%d\n", num);
+printf("%f\n", floatNum);
+printf("%c\n", letter);
+
+return 0;
+
+}
